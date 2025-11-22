29e36310ffb0466a9466f83394fd079a0d6d43bd
arequena <alexrequenaflaquer@gmail.com>
2025-11-22 10:23:31 +0100

    fix clash starting hour and added days too

--- src/app/components/top-bar-message/top-bar-message.ts
+++ src/app/components/top-bar-message/top-bar-message.ts
@@ -1,6 +1,6 @@
 import { Component, computed, effect, inject, signal } from '@angular/core';
 import { ClashService } from '../../services/clash.service';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-top-bar-message',
@@ -10,6 +10,7 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class TopBarMessage {
   private clashService = inject(ClashService);
+  private translate = inject(TranslateService);
 
   nextClash = signal<Date | null>(null);
 
@@ -25,17 +26,22 @@ export class TopBarMessage {
 
   countdown = computed(() => {
     const target = this.nextClash();
-    if (!target) return 'Loading...';
+    if (!target) return null;
 
     const diff = target.getTime() - this.now();
 
-    if (diff <= 0) return 'Clash starting!';
+    if (diff <= 0) return this.translate.instant('');
 
-    const hours = Math.floor(diff / (1000 * 60 * 60));
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    const seconds = Math.floor((diff / 1000) % 60);
+    const totalSeconds = Math.floor(diff / 1000);
+
+    const days = Math.floor(totalSeconds / (60 * 60 * 24));
+    const hours = Math.floor((totalSeconds / (60 * 60)) % 24);
+    const minutes = Math.floor((totalSeconds / 60) % 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
 
     return (
+      `${days ? `${days.toString()}d ` : ''}` +
       `${hours.toString().padStart(2, '0')}h ` +
       `${minutes.toString().padStart(2, '0')}m ` +
       `${seconds.toString().padStart(2, '0')}s`
@@ -49,7 +55,13 @@ export class TopBarMessage {
   private loadNextClash() {
     this.clashService.getNextClash().subscribe({
       next: (clash) => {
-        this.nextClash.set(new Date(clash.schedule[0]?.registrationTime));
+        const raw = clash.schedule[0]?.registrationTime;
+        if (!raw) {
+          this.nextClash.set(null);
+          return;
+        }
+
+        this.nextClash.set(new Date(raw));
       },
       error: () => {
         this.nextClash.set(null);
