29742ab45d6dc0416cbd4dd64a1a095efa9c7097
alexr <alexrequenaflaquer@gmail.com>
2025-12-26 14:25:37 +0100

    [feature/player-detail] added border levels and champion masteries wip

---
+++ src/app/components/champion-mastery-card/champion-mastery-card.ts
@@ -0,0 +1,21 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { ChampionMastery } from '../../models/player.model';
+import { ChampionsService } from '../../services/champion.service';
+
+@Component({
+  selector: 'app-champion-mastery-card',
+  imports: [],
+  templateUrl: './champion-mastery-card.html',
+  styleUrl: './champion-mastery-card.css',
+})
+export class ChampionMasteryCard {
+  private readonly champService = inject(ChampionsService);
+
+  readonly mastery = input<ChampionMastery>();
+
+  splashUrl = computed(() => {
+    const championId = this.mastery()?.championId;
+    if (!championId) return '';
+    return this.champService.getChampionSplashById(championId);
+  });
+}
